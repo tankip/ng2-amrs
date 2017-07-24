b438b36a7e6be3c0f7ef4e79488302eb9d557601
Rodgers Tanui <rtankip@gmail.com>
2017-07-24 14:39:24 +0300

    NGPOC-355: Create a functionality to discontinue an order.

--- src/app/openmrs-api/order-resource.service.ts
+++ src/app/openmrs-api/order-resource.service.ts
@@ -8,9 +8,15 @@ import * as _ from 'lodash';
 export class OrderResourceService {
 
   v: string = 'custom:(display,uuid,orderNumber,orderType,accessionNumber,' +
+<<<<<<< 2c67714140e69ade5c759403c78bab0e435f03b6
   'orderReason,orderReasonNonCoded,urgency,action,' +
   'commentToFulfiller,dateActivated,instructions,orderer:default,' +
   'encounter:full,patient:full,concept:ref)';
+=======
+  'orderReason,orderReasonNonCoded,urgency,careSetting,action,' +
+  'commentToFulfiller,dateActivated,dateStopped,instructions,orderer:default,' +
+  'encounter:full,patient:default,concept:ref)';
+>>>>>>> NGPOC-355: Create a functionality to discontinue an order.
 
   constructor(protected http: Http,
               protected appSettingsService: AppSettingsService) {
@@ -50,7 +56,26 @@ export class OrderResourceService {
       return response.json();
     });
   }
-  public getOrderByUuid(uuid: string, cached: boolean = false, v: string = null): Observable<any> {
+
+  getAllOrdersByPatientUuuid(patientUuid: string, careSettingUuid: string, cached: boolean = false,
+    v: string = null): Observable<any> {
+
+    let url = this.getUrl();
+
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('patient', patientUuid);
+    params.set('careSetting', careSettingUuid);
+    params.set('status', 'any');
+
+    params.set('v', (v && v.length > 0) ? v : this.v);
+    return this.http.get(url, {
+      search: params
+    }).map((response: Response) => {
+      return response.json();
+    });
+  }
+
+  getOrderByUuid(uuid: string, cached: boolean = false, v: string = null): Observable<any> {
 
     let url = this.getUrl();
     url += '/' + uuid;
@@ -68,9 +93,7 @@ export class OrderResourceService {
   }
 
    saveDrugOrder(payload) {
-    if (payload) {
-      console.log('Payload', payload);
-    }
+
     let url = this.getUrl();
 
     let headers = new Headers({ 'Content-Type': 'application/json' });
