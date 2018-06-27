324bae652033784c79b07f850b1d2fcc15dc81bc
Rodgers Tanui <rtankip@gmail.com>
2018-06-27 10:52:21 +0300

    CDM-16: Created DM/HTN Summary

--- src/app/patient-dashboard/cdm/patient-dashboard-cdm.module.ts
+++ src/app/patient-dashboard/cdm/patient-dashboard-cdm.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import {
+  AccordionModule, DataTableModule, SharedModule, TabViewModule,
+  GrowlModule, PanelModule, ConfirmDialogModule, ConfirmationService,
+  DialogModule, InputTextModule, MessagesModule, InputTextareaModule,
+  DropdownModule, ButtonModule, CalendarModule
+} from 'primeng/primeng';
 import { CdmLandingPageComponent } from './landing-page/landing-page.component';
 import { NgamrsSharedModule } from '../../shared/ngamrs-shared.module';
 
@@ -9,18 +14,25 @@ import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
 import { Router } from '@angular/router';
 import { SessionStorageService } from '../../utils/session-storage.service';
 import { HttpClient } from '../../shared/services/http-client.service';
+import { CdmSummaryComponent } from './cdm-summary/cdm-summary.component';
+import { CdmSummaryLatestComponent } from './cdm-summary/cdm-summary-latest.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     NgamrsSharedModule,
+    PanelModule,
+    TabViewModule
   ],
   exports: [
-    CdmLandingPageComponent
+    CdmLandingPageComponent,
+    CdmSummaryComponent
   ],
   declarations: [
-    CdmLandingPageComponent
+    CdmLandingPageComponent,
+    CdmSummaryComponent,
+    CdmSummaryLatestComponent
   ],
   providers: [
     {
